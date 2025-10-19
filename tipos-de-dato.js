1a18eb76921ba3117eafeefb1bfbd93c3df27631
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2025-10-19 15:58:26 -0400

    Primer Commit Java

---
+++ tipos-de-dato.js
@@ -0,0 +1,23 @@
+// tipos-de-dato.js
+
+// Números
+console.log(typeof 42);            // number
+console.log(typeof -666);          // number
+console.log(typeof 0);             // number
+
+// Cadenas de texto (string)
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof '');            // string
+
+
+// Booleanos
+console.log(typeof true);          // boolean
+console.log(typeof false);         // boolean
+console.log(typeof FALSE);       // string
+
+// null (tipo especial)
+console.log(typeof null);  
+
+// undefined
+console.log(typeof undefined);     // undefined
+console.log(typeof $);
