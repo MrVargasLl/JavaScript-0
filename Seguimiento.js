2350202c9654cc710d8d8a8816ba8a7763d9a5d6
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2025-11-02 00:39:58 -0400

    Funcion de seguimiento Rev 2

--- Seguimiento.js
+++ Seguimiento.js
@@ -1,116 +1,52 @@
-//Crear objeto de libros para leer
+// Crear objetos de libros deseados
+const Libro1 = { Titulo: 'Beloved', Autor: 'Toni Morrison' };
+const Libro2 = { Titulo: 'Azul', Autor: 'Rubén Darío' };
+const Libro3 = { Titulo: 'Pedro Páramo', Autor: 'Juan Rulfo' };
+const Libro4 = { Titulo: 'Aura', Autor: 'Carlos Fuentes' };
 
-//Deseo leer 1
+let Deseos = [Libro1, Libro2, Libro3, Libro4];
 
-const Libro1 = new Object();
+console.log('Total de libros que desea leer:');
+Deseos.forEach(libro => {
+    console.log('Título:', libro.Titulo, 'Autor:', libro.Autor);
+});
 
-Libro1.Titulo = 'Beloved';
+// Crear objetos de libros leídos
+const Beloved = { Titulo: 'Beloved', Autor: 'Toni Morrison' };
+const PedroPáramo = { Titulo: 'Pedro Páramo', Autor: 'Juan Rulfo' };
 
-Libro1.Autor = 'Toni Morrison';
+let Libros = [Beloved, PedroPáramo];
 
-//Deseo leer 2
-
-const Libro2 = new Object();
-
-Libro2.Titulo = 'Azul';
-
-Libro2.Autor = 'Rubén Darío';
-
-//Deseo leer 3
-
-const Libro3 = new Object();
-
-Libro3.Titulo = 'Pedro Páramo';
-
-Libro3.Autor = 'Juan Rulfo';
-
-//Deseo leer 4
-
-const Libro4 = new Object();
-
-Libro4.Titulo = 'Aura';
-
-Libro4.Autor = 'Carlos Fuentes';
-
-let Deseos = [Libro1, Libro2, Libro3, Libro4]
-
-
-console.log('Total de libros que desea leer: ');
-
-Deseos.forEach(function(E1,i,Seguimientos) {
-console.log('Titulo:', E1.Titulo,'   Autor:', E1.Autor);})
-
-//console.log('Total de libros que desea leer: ');
-
-//console.log(Deseos);
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Crea objeto de libros leidos.
-
-// Leido 1
-
-const Beloved = new Object();
-
-Beloved.Titulo = 'Beloved';
-
-Beloved.Autor = 'Toni Morrison';
-
-//leido 2
-
-const PedroPáramo = new Object();
-
-PedroPáramo.Titulo = 'Pedro Páramo';
-
-PedroPáramo.Autor = 'Juan Rulfo';
-
-//Leido 3
-
-
-//Declarar un arreglo de objetos llamado Libros con varios datos de diferentes Libros leidos.
-
-let Libros = [Beloved, PedroPáramo]
-
-//console.log('Leidos: ');
-
-//console.log(Libros);
-
-//Agregar un libro al array
-
-function agregarLibro(Titulo,Autor,book) {
-    const libro = new Object();
-    libro.Titulo=Titulo;
-    libro.Autor=Autor
+// Agregar un libro a la lista de leídos
+function agregarLibro(Titulo, Autor, book) {
+    const libro = { Titulo, Autor };
     book.push(libro);
-    console.log('Libro añadido: ');
-    console.log('Titulo:', libro.Titulo, 'Autor:',libro.Autor );
+    console.log('Libro añadido:');
+    console.log('Título:', libro.Titulo, 'Autor:', libro.Autor);
 }
 
-agregarLibro('Aura', 'Carlos Fuentes',Libros)
+agregarLibro('Aura', 'Carlos Fuentes', Libros);
 
-//Declarar un arreglo de objetos "Libros".
-
-let Seguimientos = [ ]
-
-// Función de Seguimiento de libros
-
-function Seguimiento(Deseo,Cumplidos,Segui) {
-    Deseo.forEach(function(E1,i,Deseo) {
-        Cumplidos.forEach(function(E2,j,Cumplidos){
-            if (E1.Titulo === E2.Titulo && E1.Autor === E2.Autor) {
-                Segui.push(E1)
+// Función de seguimiento de libros leídos
+function Seguimiento(Deseo, Cumplidos, Segui) {
+    Deseo.forEach(libroDeseado => {
+        Cumplidos.forEach(libroLeido => {
+            if (libroDeseado.Titulo === libroLeido.Titulo && libroDeseado.Autor === libroLeido.Autor) {
+                Segui.push(libroDeseado);
             }
-        })
-    })
+        });
+    });
 }
 
-function mostrarLibrosLeidos () {
-
-    Seguimiento(Deseos,Libros,Seguimientos);
-
-console.log('Solamente ha leido: ');
-Seguimientos.forEach(function(E1,i,Seguimientos) {
-console.log('Titulo:', E1.Titulo,'   Autor:', E1.Autor);})
+// Mostrar los libros leídos
+function mostrarLibrosLeidos() {
+    let Seguimientos = [];
+    Seguimiento(Deseos, Libros, Seguimientos);
+    
+    console.log('Solamente ha leído:');
+    Seguimientos.forEach(libro => {
+        console.log('Título:', libro.Titulo, 'Autor:', libro.Autor);
+    });
 }
 
-mostrarLibrosLeidos ()
+mostrarLibrosLeidos();
